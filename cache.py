83167ebf267f3908a59fe0bf4d9de2e6e9d35f8d
Nikita Lesenkovs <144688383+Nikitalesenkovs@users.noreply.github.com>
2026-03-14 19:19:16 +0200

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,7 +1,9 @@
 seen_items = set()
 
-def is_seen(item_id):
+
+def is_seen(item_id: str) -> bool:
     return item_id in seen_items
 
-def mark_seen(item_id):
+
+def mark_seen(item_id: str) -> None:
     seen_items.add(item_id)
